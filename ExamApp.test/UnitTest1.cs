c5de2dd31e5f05e6ec57a57d617476b8c5a8f1f5
robin.malpart <malpart.robin@gmail.com>
2025-02-12 14:49:20 +0100

    implement grid generation, and add first TDD step to PlaceNQueen method

--- ExamApp.test/UnitTest1.cs
+++ ExamApp.test/UnitTest1.cs
@@ -1,14 +1,17 @@
 using System;
 using Xunit;
+using ExamApp;
 
 namespace ExamApp.test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestPlaceNQueen_1x1Grid()
         {
-
+            ChessGrid chessGrid = new ChessGrid(1);
+            chessGrid.PlaceNQueen();
+            Assert.Equal('#', chessGrid.GetCell(0, 0));
         }
     }
 }
--- ExamApp/Program.cs
+++ ExamApp/Program.cs
@@ -4,9 +4,52 @@ namespace ExamApp
 {
     class Program
     {
+        private char[,] grid;
+        private int size;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program(10);
+            program.PlaceNQueen();
+            program.DisplayGrid();
+        }
+
+        public Program(int n)
+        {
+            size = n;
+            grid = new char[n, n];
+            InitializeGrid();
+        }
+
+        private void InitializeGrid()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    grid[i, j] = '0';
+                }
+            }
+        }
+
+        public void PlaceNQueen()
+        {
+            if (size == 1)
+            {
+                grid[0, 0] = '#';
+            }
+        }        
+
+        public void DisplayGrid()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(grid[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
